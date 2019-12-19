50b2da4e804fa43ca37d337bd30db376636f89a3
Pranav Khandelwal <pranavkhandelwal@Pranavs-MacBook-Air.local>
2019-12-19 19:13:13 +0530

    recursion

--- Lecture 3/recursion_2nd.cpp
+++ Lecture 3/recursion_2nd.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include<climits>
 
 using namespace std;
 
@@ -85,6 +86,87 @@ void mappedString(string str,string ans){
 
 string keycode[] = {".","#","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
 
+void printKeypad(string str,string ans){
+	if(str.length()==0){
+		cout<<ans<<endl;
+		return;
+	}
+
+	char ch = str[0];
+	string ros = str.substr(1);
+
+	string key = keycode[ch - '0'];
+
+	for(int i=0;i<key.length();i++){
+		printKeypad(ros,ans + key[i]);
+	}
+}
+
+int reduceToOne(int n){
+	if(n==1){
+		return 0;
+	}
+
+	int count1 = INT_MAX,count2 = INT_MAX,count3 = INT_MAX;
+
+	if(n%3==0){
+		count3 = reduceToOne(n/3) + 1;
+	}
+
+	if(n%2==0){
+		count2 = reduceToOne(n/2) + 1;
+	}
+
+	count1 = reduceToOne(n-1) + 1;
+
+	return min(count1,min(count2,count3));
+}
+
+int perfectSquare(int n){
+	if(n<=0){
+		return 0;
+	}
+
+	int minValue = INT_MAX;
+
+	int square = 1;
+
+	for(int i=1;square<=n;i++){
+		minValue = min(minValue,perfectSquare(n-square) + 1);
+		square = i*i;
+	}
+
+	return minValue;
+}
+
+bool partitionEqualSumSubset(int arr[],int n,int si,int sum,int total){
+	if(2*sum==total){
+		return true;
+	}
+
+	if(sum>total or si>=n){
+		return false;
+	}
+
+	bool include = partitionEqualSumSubset(arr,n,si+1,sum+arr[si],total);
+	bool exclude = partitionEqualSumSubset(arr,n,si+1,sum,total);
+
+	return exclude||include;
+}
+
+bool isPossible(int arr[],int n){
+	int total = 0;
+
+	for(int i=0;i<n;i++){
+		total+=arr[i];
+	}
+
+	if(total&1){
+		return false;
+	}
+
+	return partitionEqualSumSubset(arr,n,0,0,total);
+}
 
 int main(){
 
@@ -102,7 +184,18 @@ int main(){
 
 	// cout<<(a)<<endl;
 
-	mappedString("1234","");
+	// mappedString("1234","");
+
+	// printKeypad("23","");
+
+	// cout<<reduceToOne(21)<<endl;
+
+	// cout<<perfectSquare(13)<<endl;
+
+	int arr[] = {1,11,5,5};
+	int n = 4;
+
+	cout<<isPossible(arr,n)<<endl;
 
 	return 0;
 }
