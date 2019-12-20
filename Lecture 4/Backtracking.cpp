9fd3c520687fcc13656835818510dff6e70c19cd
Pranav Khandelwal <pranavkhandelwal@Pranavs-MacBook-Air.local>
2019-12-20 17:45:21 +0530

    backtracking

--- Lecture 4/Backtracking.cpp
+++ Lecture 4/Backtracking.cpp
@@ -49,7 +49,7 @@ bool NQueens(int board[][4],int row,int n){
 			cout<<endl;
 		}
 		cout<<"***************"<<endl;
-		return false;
+		return true;
 	}
 
 	for(int col=0;col<n;col++){
@@ -70,12 +70,93 @@ bool NQueens(int board[][4],int row,int n){
 	return false;
 }
 
+void printNQueens(int board[][4],int row,int n){
+	if(row==n){
+		for(int i=0;i<n;i++){
+			for(int j=0;j<n;j++){
+
+				if(board[i][j]){
+					cout<<"Q ";
+				}else{
+					cout<<"_ ";
+				}
+			}
+			cout<<endl;
+		}
+		cout<<"***************"<<endl;
+		return;
+	}
+
+	for(int col=0;col<n;col++){
+		if(isSafe(board,row,col,n)){
+
+			board[row][col]=1;
+
+			printNQueens(board,row+1,n);
+
+			board[row][col] = 0;
+		}
+	}
+
+	return;
+}
+
+int countNQueens(int board[][4],int row,int n){
+	if(row==n){
+		return 1;
+	}
+
+	int count = 0;	
+
+	for(int col=0;col<n;col++){
+		if(isSafe(board,row,col,n)){
+
+			board[row][col]=1;
+
+			count+=countNQueens(board,row+1,n);
+
+			board[row][col] = 0;
+		}
+	}
+
+	return count;
+}
+
 int main(){
 
 	int board[4][4] = {0};
 	int n = 4;
 
-	cout<<NQueens(board,0,n)<<endl;
+	// cout<<NQueens(board,0,n)<<endl;
+
+	// printNQueens(board,0,n);
+
+	// cout<<countNQueens(board,0,n)<<endl;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	char maze[][4] = {
+     	{'0','0','0','0'},
+     	{'0','0','0','0'},
+     	{'0','0','X','0'},
+     	{'0','X','0','0'},
+     };
+
+     int sol[4][4] = {0};
+
+     // cout<<ratInMaze(maze,sol,0,0,3,3)<<endl;
 
 	return 0;
 }
