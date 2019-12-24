398ed8a7c11ce7c0a03972963a5ece142d05ca5d
Pranav Khandelwal <pranavkhandelwal@Pranavs-MacBook-Air.local>
2019-12-24 19:20:20 +0530

    divide and conquer

--- Lecture 6/divide_and_conquer_2.cpp
+++ Lecture 6/divide_and_conquer_2.cpp
@@ -53,15 +53,159 @@ int aggressiveCows(int arr[],int n,int cows){
 	return ans;
 }
 
+bool canGive(int n,int x,int m,int mid,int y){
+	return mid*x <= m + (n-mid)*y;
+}
 
+int winningCBScholarship(int n,int m,int x,int y){
+
+	int minStudent = 0;
+	int maxStudent = n; 
+
+	int ans = 0;
+
+
+	while(minStudent<=maxStudent){
+
+		int mid = minStudent + (maxStudent - minStudent)/2;
+
+		if(canGive(n,x,m,mid,y)){
+			minStudent = mid + 1;
+			ans = mid;
+		}else{
+			maxStudent = mid - 1;
+		}
+	}
+
+	return ans;
+}
+
+void merge(int arr[],int s,int e){
+
+	int mid = s + (e-s)/2;
+
+	int i = s;
+	int j = mid + 1;
+
+	int temp[e-s+1];
+	int k =0;
+
+	while(i<=mid and j<=e){
+
+		if(arr[i]<=arr[j]){
+			temp[k++] = arr[i++];
+		}else{
+			temp[k++] = arr[j++];
+		}
+	}
+
+	while(i<=mid){
+		temp[k++] = arr[i++];
+	}
+
+	while(j<=e){
+		temp[k++] = arr[j++];
+	}
+
+	int p = 0;
+
+	for(int x = s;x <= e;x++){
+		arr[x] = temp[p];
+		p++;
+	}
+}
+
+void mergeSort(int arr[],int s,int e){
+	if(s==e){
+		return;
+	}
+
+	int mid = s + (e-s)/2;
+
+	mergeSort(arr,s,mid);
+	mergeSort(arr,mid+1,e);
+
+	merge(arr,s,e);
+}
+
+void quickSort(int arr[],int low,int high){
+	if(low>=high){
+		return;
+	}
+
+	int left = low;
+	int right = high;
+
+	int mid = (low+high)/2;
+
+	int pivot = arr[mid];
+
+	while(left<=right){
+
+		while(arr[left]<pivot){
+			left++;
+		}
+
+		while(arr[right]>pivot){
+			right--;
+		}
+
+		if(left<=right){
+			swap(arr[left],arr[right]);
+			left++;
+			right--;
+		}
+	}
+
+	for(int i=0;i<6;i++){
+		cout<<arr[i]<<" ";
+	}
+	cout<<endl<<"****************"<<endl;
+
+	quickSort(arr,low,right);
+	quickSort(arr,left,high);
+}
 
 int main(){
 
-	int arr[] = {2,4,1,8,9};
-	int n = 5;
-	int cows = 3;
+	// int arr[] = {2,4,1,8,9};
+	// int n = 5;
+	// int cows = 3;
 
-	cout<<aggressiveCows(arr,n,cows)<<endl;
+	// cout<<aggressiveCows(arr,n,cows)<<endl;
+
+	// cout<<winningCBScholarship(5,10,2,1)<<endl;
+
+	// int arr[] = {6,5,4,3,2,1};
+	// int n = 6;
+
+	// mergeSort(arr,0,n-1);
+
+	// for(int i=0;i<n;i++){
+	// 	cout<<arr[i]<<" ";
+	// }
+	// cout<<endl;
+
+	int arr[] = {10, 7, 8, 9, 1, 5};
+	int n = 6;
+
+	cout<<"BEFORE SORT"<<endl;
+
+	for(int i=0;i<n;i++){
+		cout<<arr[i]<<" ";
+	}
+	cout<<endl;
+
+	cout<<"WHILE SORTING"<<endl;
+
+	quickSort(arr,0,n-1);
+
+	cout<<"AFTER SORT"<<endl;
+
+	for(int i=0;i<n;i++){
+		cout<<arr[i]<<" ";
+	}
+	cout<<endl;
 
 	return 0;
 }
