f0df7a3de414b867a2467730ebb2a47cecc70c0d
khandelwalpranav05 <khandelwalpranav1996@gmail.com>
2020-01-03 15:25:06 +0530

    dp

--- Lecture 9/dp_basics.cpp
+++ Lecture 9/dp_basics.cpp
@@ -681,6 +681,32 @@ int longestPalindromeSubseq(string s) {
 
 // Longest Palindromic Subsequence With Pure DP
 
+int longestPalindromeSubseqDP(string s) {
+        
+    int n = s.length();
+        
+    int dp[n][n];
+        
+    memset(dp,0,sizeof(dp));
+        
+    for(int i=0;i<n;i++){
+        dp[i][i] = 1;
+    }
+        
+    for(int col=1;col<n;col++){
+        for(int row=col-1;row>=0;row--){
+                
+            if(s[row]==s[col]){
+                dp[row][col] = dp[row+1][col-1] + 2;
+            }else{
+                dp[row][col] = max(dp[row][col-1],dp[row+1][col]);
+            }
+        }
+    }
+        
+    return dp[0][n-1];   
+}
+
 int main(){
 
 //*************FIBONACCI************
@@ -789,6 +815,6 @@ int main(){
 // ************ LONGEST PALINDROMIC SUBSEQUENCE ***********
 
 	cout<<longestPalindromeSubseq("abeghebcg")<<endl;
-
+	cout<<longestPalindromeSubseqDP("abeghebcg")<<endl;
 	return 0;
 }
