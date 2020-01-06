6da074dfff7f56576230068d07f429b96a052385
khandelwalpranav05 <khandelwalpranav1996@gmail.com>
2020-01-06 15:15:49 +0530

    trees

--- Lecture 12-13/binaryTree.cpp
+++ Lecture 12-13/binaryTree.cpp
@@ -102,14 +102,84 @@ bool search(node* root,int item){
 	return leftSearch or rightSearch;
 }
 
-// int height(node* root){
+int height(node* root){
+	if(root==NULL){
+		return -1;
+	}
 
+	int leftHeight = height(root->left);
+	int rightHeight = height(root->right);
 
-// }
+	return max(leftHeight,rightHeight) + 1;
+}
 
-// int diameter(node*root){
+int diameter(node*root){
+	if(root==NULL){
+		return 0;
+	}
 
-// }
+	int leftDiameter = diameter(root->left);
+	int rightDiameter = diameter(root->right);
+
+	int leftHeight = height(root->left);
+	int rightHeight = height(root->right);
+
+	int totalDiamter = max(leftHeight+rightHeight+2,max(leftDiameter,rightDiameter));
+	return totalDiamter;
+}
+
+bool isBalanced(node*root){
+	if(root==NULL){
+		return true;
+	}
+
+	bool leftBalanced = isBalanced(root->left);
+	bool rightBalanced = isBalanced(root->right);
+
+	if(!leftBalanced or !rightBalanced){
+		return false;
+	}
+
+	int leftHeight = height(root->left);
+	int rightHeight = height(root->right);
+
+	int gap = abs(leftHeight - rightHeight);
+
+	if(gap>1){
+		return false;
+	}else{
+		return true;
+	}
+}
+
+class DiaHeight{
+public:
+	int height;
+	int diameter;
+};
+
+DiaHeight diameterOptimized(node*root){
+	DiaHeight val;
+	if(root==NULL){
+		val.diameter = 0;
+		val.height = -1;
+		return val;
+	}
+
+	DiaHeight leftPair = diameterOptimized(root->left);
+	DiaHeight rightPair = diameterOptimized(root->right);
+
+	int leftDiameter = leftPair.diameter;
+	int rightDiameter = rightPair.diameter;
+	int throughRoot = leftPair.height + rightPair.height + 2;
+
+	val.diameter = max(throughRoot,max(leftDiameter,rightDiameter));
+
+	val.height = max(leftPair.height,rightPair.height) + 1;
+
+	return val;
+}
+
 
 int main(){
 
@@ -117,12 +187,21 @@ int main(){
 
 	root = constructTree();
 
-	preorder(root);
-	cout<<endl;
+	// preorder(root);
+	// cout<<endl;
 
 	// cout<<countNodes(root)<<endl;
 	// cout<<sumOfNodes(root)<<endl;
 	// cout<<search(root,2)<<endl;
+
+	// cout<<height(root)<<endl;
+
+	// cout<<diameter(root)<<endl;
+
+	// cout<<isBalanced(root)<<endl;
+
+	DiaHeight val = diameterOptimized(root);
+	cout<<val.diameter<<endl;
 
 	return 0;
 }
@@ -132,3 +211,4 @@ int main(){
 // FOR DIAMETER TESTING
 // 8 4 2 1 -1 -1 -1 3 -1 -1 5 -1 6 -1 -1
 // 4 2 8 9 10 -1 -1 -1 -1 7 -1 3 -1 4 -1 6 -1 -1 5 -1 -1
+// 1 -1 2 -1 3 -1 4 -1 5 -1 -1
