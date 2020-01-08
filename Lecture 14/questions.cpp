6c4768d98f9a1b91801d371c2a426be491c6b8b2
khandelwalpranav05 <khandelwalpranav1996@gmail.com>
2020-01-08 17:57:52 +0530

    hashmap

--- Lecture 14/questions.cpp
+++ Lecture 14/questions.cpp
@@ -49,18 +49,51 @@ int longestSubarraySumWithZero(int arr[],int n){
 	return maxLength;
 }
 
+int longestSubarraySumWithK(int arr[],int n,int k){
+
+	//TODO
+
+	return 0;
+}
+
 int verticalOrder(){
 
 	unordered_map<int,list<int> > h;
+	return 0;
+}
 
+int subarraySum(vector<int>& nums, int k) {
+            
+    unordered_map<int,int> h;
+    int ans = 0;
+    int sum = 0;
+        
+    h[0] = 1;
+        
+    for(int i=0;i<nums.size();i++){
+        sum+=nums[i];
+            
+        if(h.count(sum - k)){
+            ans+=h[sum-k];
+        }
+            
+        h[sum]+=1;
+    }
+        
+    return ans;
 }
 
 int main(){
 
-	int arr[] = {1,2,3,-3,-2,-1};
-	int n = 6;
+	// int arr[] = {1,2,3,-3,-2,-1};
+	// int n = 6;
+	// cout<<longestSubarraySumWithZero(arr,n)<<endl;
 
-	cout<<longestSubarraySumWithZero(arr,n)<<endl;
+	// int arr[] = {1,-1,54,-51,-1,-3};
+	// int n = 6;
+	// int k = -1;
+
+	// cout<<longestSubarraySumWithK(arr,n,k)<<endl;
 
 	return 0;
 }
