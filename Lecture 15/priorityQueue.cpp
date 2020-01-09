d87552b6f3ab6e3f9407d4a4964bc7343becc13b
khandelwalpranav05 <khandelwalpranav1996@gmail.com>
2020-01-09 17:08:39 +0530

    heap

--- Lecture 15/priorityQueue.cpp
+++ Lecture 15/priorityQueue.cpp
@@ -5,7 +5,21 @@ using namespace std;
 
 int main(){
 
-	
+	priority_queue<int,vector<int>,greater<int> > pq;
+
+	pq.push(6);
+	pq.push(4);
+	pq.push(3);
+	pq.push(5);
+	pq.push(1);
+
+	cout<<pq.top()<<endl;
+
+	pq.pop();
+
+	cout<<pq.size()<<endl;
+
+	cout<<pq.top()<<endl;
 
 	return 0;
 }
